23c8a0f4f1484e890fb614932f030969bfe9266b
peige <1263292336@qq.com>
2017-07-10 23:34:37 +0800

    Unit21 Executor

---
+++ src/unit21/MoreBasicThread.java
@@ -0,0 +1,13 @@
+package unit21;
+
+public class MoreBasicThread {
+
+	public static void main(String[] args) {
+		
+		for(int i = 0; i < 5; ++i) {
+			new Thread(new LiftOff()).start();
+		}
+		
+		System.out.println("Waiting for LiftOff");
+	}
+}
